f52806da6af5cabb7e40de792b8c7b2bf69197eb
Bo <bhsuhkr@naver.com>
2021-01-19 23:08:40 -0600

    optimization

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -6,6 +6,7 @@ import { error } from 'protractor';
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
